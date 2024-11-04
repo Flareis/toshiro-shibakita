fa1e12aa9f2dc1af562ff5e7a7946f8babbebd88
Flávia dos Reis <86988733+Flareis@users.noreply.github.com>
2024-11-04 16:17:02 -0300

    feat: Update banco.sql
    insert productId, prosuctName, category, price and stockQuantity

--- banco.sql
+++ banco.sql
@@ -1,8 +1,7 @@
-CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+CREATE TABLE mkt_Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(50),
+    Category VARCHAR(50),
+    Price DECIMAL(10, 2),
+    StockQuantity INT
 );
